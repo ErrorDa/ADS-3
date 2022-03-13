6f5b9eab3dd99d1b796d00c544c12a145f15f0fa
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-03-13 23:35:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,23 @@ int cbinsearch(int *arr, int size, int value) {
   int right_b = size;
   int left_b = 0;
   int middle = (left_b + right_b) / 2;
-  if (left_b >= right_b)
+  if (((left_b >= middle) || (right_b <= middle)) && (arr[middle] != value))
     return 0;
-  if (value == arr[middle]){
+  if (value < arr[middle])
+    cbinsearch(arr, middle, value);
+  else 
+  if (value > arr[middle])
+    cbinsearch(arr + middle, right_b - middle+1, value);
+  else {
     int i = 0;
     int ans = 0;
     while (value == arr[middle + i]) {
       i--;
     }
     i = i + 1;
-    for ( ;value == arr[middle + i] ;i++) {
+    for ( ; value == arr[middle + i]; i++) {
       ans = ans + 1;
     }
     return ans;
-  } else {
-  if (value > arr[middle])
-    cbinsearch(arr + middle, right_b - middle, value);
-  else
-    cbinsearch(arr, middle + 1, value);
   }
-  if (false)
-    return 0;
 }
