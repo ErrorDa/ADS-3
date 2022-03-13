1b8e8af00fe87ba4d23352680b54c66297f91f7b
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-03-13 21:18:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,26 @@
-// Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right_b = size-1;
+  int left_b = 0;
+  int middle = (left_b + right_b) / 2;
+  if (left_b >= right_b)
+    return 0;
+  if (value == arr[middle]) {
+    int i = 0;
+    int ans = 0;
+    while (value == arr[middle + i]) {
+      i--;
+    }
+    i++;
+    for (;value == arr[middle + i] ;i++) {
+      ans++;
+    }
+    return ans;
+  }
+  else if (value > arr[middle]) {
+    cbinsearch(arr + middle, right_b - middle, value);
+  }
+  else {
+    cbinsearch(arr, middle + 1, value);
+  }
 }
